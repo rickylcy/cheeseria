fa35cf7a7da7ae324647d49aab99a2ef4427ed64
rickylcy <94838682+rickylcy@users.noreply.github.com>
2022-08-04 22:23:08 +1000

    Update db connection with env variables

--- src/server/connect.ts
+++ src/server/connect.ts
@@ -1,22 +1,12 @@
 const mysql = require("mysql");
 require('dotenv').config();
 
-/*
-// local db
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-*/
-
-const connection = mysql.createConnection({
-  host: 'cheeseria-1.cbknyf9gvk5y.us-east-1.rds.amazonaws.com',
-  database: 'cheeseria',
-  user: 'admin',
-  password: 'password',
-  port: '3306'
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err:Error) => {
